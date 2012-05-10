70346bcadbeabae6b281830ad222180e05ead8ef
Julian Amaya <julian@monoku.com>
2012-05-09 20:02:43 -0500

    quito la dependencia de south

--- simplecrm/settings.py
+++ simplecrm/settings.py
@@ -122,7 +122,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    'south',
     'customers',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
